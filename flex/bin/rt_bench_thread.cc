308e74df0c60c98520d6aee5f23ca92cc93e4a25
zondayc <2645828175@qq.com>
2024-12-19 14:41:03 +0800

    Add batch interface in read transaction

--- flex/bin/rt_bench_thread.cc
+++ flex/bin/rt_bench_thread.cc
@@ -121,7 +121,14 @@ class Req {
       if (length == 0)
         assert(false);
       ::fread(buffer.data(), length, 1, query_file_string);
-      reqs_.emplace_back(std::string(buffer.data(), buffer.data() + length));
+      auto query=std::string(buffer.data(), buffer.data() + length);
+      auto type=int(query.back());
+      if(type==9){
+        reqs_.emplace_back(std::string(buffer.data(), buffer.data() + length));
+      }
+      if (reqs_.size() > 100) {
+        break;
+      }
     }
     num_of_reqs_unique_ = reqs_.size();
     LOG(INFO) << "Number of query = " << num_of_reqs_unique_;
--- flex/engines/graph_db/database/read_transaction.h
+++ flex/engines/graph_db/database/read_transaction.h
@@ -16,12 +16,16 @@
 #ifndef GRAPHSCOPE_DATABASE_READ_TRANSACTION_H_
 #define GRAPHSCOPE_DATABASE_READ_TRANSACTION_H_
 
+#include <array>
 #include <limits>
+#include <tuple>
 #include <utility>
+#include <vector>
 
 #include "flex/storages/rt_mutable_graph/mutable_csr.h"
 #include "flex/storages/rt_mutable_graph/mutable_property_fragment.h"
 #include "flex/storages/rt_mutable_graph/types.h"
+#include "flex/utils/id_indexer.h"
 
 namespace gs {
 
@@ -320,12 +324,220 @@ class ReadTransaction {
     return SingleGraphView<EDATA_T>(*csr, timestamp_);
   }
 
+  /*
+  批量获取顶点ID
+  @param label: 顶点标签
+  @param indices: 顶点索引组成的vector
+  @return: 顶点ID组成的vector
+  */
+  std::vector<oid_t> BatchGetVertexIds(label_t label,
+                                       const std::vector<vid_t>& indices) const;
+
+  /*
+  批量查找顶点索引
+  @param label: 顶点标签
+  @param oids: 顶点ID组成的vector
+  @return: 顶点索引组成的vector和是否存在的vector
+  */
+  std::pair<std::vector<vid_t>, std::vector<bool>> BatchGetVertexIndices(
+      label_t label, const std::vector<oid_t>& oids) const;
+
+  /*
+  批量获取顶点属性
+  @param label: 顶点标签
+  @param vids: 顶点ID组成的vector
+  @param col_ids: 属性ID组成的vector
+  @return: 顶点属性组成的vector
+  */
+  // template <typename... T>
+  // std::vector<std::tuple<T...>> BatchGetVertexPropsFromVid(
+  //     label_t label, const std::vector<vid_t>& vids,
+  //     const std::array<int, sizeof...(T)>& col_ids) const {
+  //   std::vector<std::tuple<T...>> results(vids.size());
+  //   auto& table = graph_.get_vertex_table(label);
+    
+  //   // 获取所有列
+  //   std::array<const ColumnBase*, sizeof...(T)> columns;
+  //   for(size_t i = 0; i < sizeof...(T); i++) {
+  //     columns[i] = table.get_column_by_id(col_ids[i]);
+  //     CHECK(columns[i] != nullptr) << "Column " << col_ids[i] << " not found";
+  //   }
+
+  //   // 为每个顶点获取属性值
+  //   for(size_t i = 0; i < vids.size(); i++) {
+  //     auto vid = vids[i];
+  //     results[i] = std::make_tuple(
+  //       std::dynamic_pointer_cast<TypedColumn<T>>(columns[std::index_sequence_for<T...>{}])->get_view(vid)...
+  //     );
+  //   }
+  //   return results;
+  // }
+
+  /*
+  批量获取顶点属性
+  @param label_id: 顶点标签
+  @param vids: 顶点ID组成的vector
+  @param prop_names: 属性名称组成的vector
+  @return: 顶点属性组成的vector
+  */
+  std::vector<gbp::BufferBlock> BatchGetVertexPropsFromVid(
+      label_t label_id, const std::vector<vid_t>& vids,
+      const std::vector<std::string>& prop_names) const {
+    std::vector<gbp::BufferBlock> results;
+    results.reserve(vids.size() * prop_names.size());  // 预分配空间
+    auto& table = graph_.get_vertex_table(label_id);
+    
+    // 获取所有列
+    std::vector<std::shared_ptr<ColumnBase>> columns;
+    columns.reserve(prop_names.size());
+    for(const auto& prop_name : prop_names) {
+      auto column = table.get_column(prop_name);
+      CHECK(column != nullptr) << "Column " << prop_name << " not found";
+      columns.push_back(column);
+    }
+
+    // 为每个顶点获取属性值
+    for(size_t i = 0; i < vids.size(); i++) {
+      auto vid = vids[i];
+      for(const auto& column : columns) {
+        auto type = column->type();
+        if(type == PropertyType::kString) {
+          auto string_column = std::dynamic_pointer_cast<StringColumn>(column);
+          CHECK(string_column != nullptr);
+          results.push_back(string_column->get(vid));
+        } else if(type == PropertyType::kDate) {
+          auto date_column = std::dynamic_pointer_cast<TypedColumn<Date>>(column);
+          CHECK(date_column != nullptr);
+          results.push_back(date_column->get(vid));
+        } else if(type == PropertyType::kInt32) {
+          auto int_column = std::dynamic_pointer_cast<TypedColumn<int>>(column);
+          CHECK(int_column != nullptr);
+          results.push_back(int_column->get(vid));
+        } else if(type == PropertyType::kInt64) {
+          auto int64_column = std::dynamic_pointer_cast<TypedColumn<int64_t>>(column);
+          CHECK(int64_column != nullptr);
+          results.push_back(int64_column->get(vid));
+        } else {
+          LOG(FATAL) << "Unsupported property type: " << static_cast<int>(type);
+        }
+      }
+    }
+    return results;
+  }
+
+  /* 批量获取出边
+  @param v_label: 顶点标签             
+  @param vids: 顶点ID组成的vector
+  @param neighbor_label: 邻居标签
+  @param edge_label: 边标签
+  @return: 出边列表
+  */
+  template <typename EDATA_T>
+  std::vector<AdjListView<EDATA_T>> BatchGetOutgoingEdges(
+      label_t v_label, const std::vector<vid_t>& vids, label_t neighbor_label,
+      label_t edge_label) const;
+
+  /* 批量获取入边
+  @param v_label: 顶点标签
+  @param vids: 顶点ID组成的vector
+  @param neighbor_label: 邻居标签
+  @param edge_label: 边标签
+  @return: 入边列表
+  */
+  template <typename EDATA_T>
+  std::vector<AdjListView<EDATA_T>> BatchGetIncomingEdges(
+      label_t v_label, const std::vector<vid_t>& vids, label_t neighbor_label,
+      label_t edge_label) const;
+
+  /* 获取边指向的邻居
+  @param src_label_id: 源顶点标签
+  @param dst_label_id: 目标顶点标签
+  @param edge_label_id: 边标签
+  @param vids: 顶点ID组成的vector
+  @param direction_str: 方向
+  @return: 邻居列表
+  */
+  std::vector<std::vector<vid_t>> GetOtherVertices(
+      const label_t& src_label_id, const label_t& dst_label_id,
+      const label_t& edge_label_id, const std::vector<vid_t>& vids,
+      const std::string& direction_str) const;
+
+
+  template <typename FUNC_T, typename... SELECTOR>
+  void ScanVertices(const std::string& label,
+                    const std::tuple<SELECTOR...>& props,
+                    const FUNC_T& func) const {
+    auto label_id = graph_.schema().get_vertex_label_id(label);
+    auto vnum = graph_.vertex_num(label_id);
+
+    // 获取属性列
+    auto columns = get_tuple_column_from_graph_with_property(label_id, props);
+    std::tuple<typename SELECTOR::prop_t...> t;
+
+    // 遍历所有顶点
+    if constexpr (sizeof...(SELECTOR) == 0) {
+      // 无属性的情况
+      for (auto v = 0; v != vnum; ++v) {
+        func(v, t);
+      }
+    } else {
+      // 有属性的情况
+      for (auto v = 0; v != vnum; ++v) {
+        get_tuple_from_column_tuple(v, t, columns);
+        func(v, t);
+      }
+    }
+  }
+
  private:
   void release();
 
   const MutablePropertyFragment& graph_;
   VersionManager& vm_;
   timestamp_t timestamp_;
+
+  template <size_t Is = 0, typename... T, typename column_tuple_t>
+  void fetch_properties_in_column(const std::vector<vid_t>& vids,
+                                  std::vector<std::tuple<T...>>& props,
+                                  column_tuple_t& column) const {
+    // auto index_seq = std::make_index_sequence<sizeof...(T)>{};
+
+    auto& cur_column = std::get<Is>(column);
+    if (cur_column) {
+      for (auto i = 0; i < vids.size(); ++i) {
+        std::get<Is>(props[i]) = cur_column->get_view(vids[i]);
+      }
+    }
+
+    if constexpr (Is + 1 < sizeof...(T)) {
+      fetch_properties_in_column<Is + 1>(vids, props, column);
+    }
+  }
+
+  // template<typename... T, size_t... Is>
+  // void get_buffers_helper(
+  //     const std::array<std::shared_ptr<ColumnBase>, sizeof...(T)>& columns,
+  //     vid_t vid,
+  //     std::index_sequence<Is...>,
+  //     std::vector<gbp::BufferBlock>& results) const {
+  //   using expander = int[];
+  //   (void)expander{0,
+  //     ((void)(results.push_back(
+  //       [&]() -> gbp::BufferBlock {
+  //         using Type = std::tuple_element_t<Is, std::tuple<T...>>;
+  //         if constexpr (std::is_same_v<Type, std::string_view>) {
+  //           auto column = std::dynamic_pointer_cast<StringColumn>(columns[Is]);
+  //           CHECK(column != nullptr);
+  //           return column->get(vid);
+  //         } else {
+  //           auto column = std::dynamic_pointer_cast<TypedColumn<Type>>(columns[Is]);
+  //           CHECK(column != nullptr);
+  //           return column->get(vid);
+  //         }
+  //       }()
+  //     )), 0)...
+  //   };
+  // }
 };
 
 }  // namespace gs
